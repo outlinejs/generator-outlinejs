ec0fb48b477d0955b8609839a168573bc7119034
Fabio Masini <fabio@rapidosoft.it>
2016-02-12 11:34:56 +0100

    Rendering hotfix!

--- generators/app/templates/project/main.js
+++ generators/app/templates/project/main.js
@@ -2,4 +2,4 @@ import RootRouter from './urls';
 import Boot from 'outlinejs/boot';
 import Settings from './settings';
 
-Boot.init(RootRouter, document.getElementById('main'), new Settings());
+Boot.init(RootRouter, 'main', new Settings());
