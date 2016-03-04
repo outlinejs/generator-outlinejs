d5f775962e3affef8c1df2304aef81a081b6c870
Carlo Bertini <waydotnet@gmail.com>
2016-03-04 09:58:24 +0100

    close #2

--- generators/app/templates/gulpfile.babel.js
+++ generators/app/templates/gulpfile.babel.js
@@ -4,10 +4,20 @@ import OutlineJsTasks from 'outlinejs/lib/utils/build/tasks';
 import browserify from 'browserify';
 import eslint from 'gulp-eslint';
 
-const ojsTasks = new OutlineJsTasks(gulp, browserify, eslint);
+class GulpTasks extends OutlineJsTasks {
+  getBrowserify(debug = false, forNode = false, watch = false, files = [this.projectJsEntry]) {
+    var b = super.getBrowserify(debug, forNode, watch, files);
+    if (forNode) {
+      //
+      b = b.exclude('jquery');
+    }
+    return b;
+  }
+}
+
+const ojsTasks = new GulpTasks(gulp, browserify, eslint);
 ojsTasks.load();
 
 gulp.task('default', ['ojs:clean'], () => {
   gulp.start('ojs:build');
 });
-
